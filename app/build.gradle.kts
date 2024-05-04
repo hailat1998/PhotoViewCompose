63cefe53cce046859bc045fb2183f6f890a44579
hailat1998 <htemesgen400@gmail.com>
2024-05-04 08:20:48 +0000

    fixed the hilt issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.kapt")
+
 }
+
 
 android {
     namespace = "com.hd.photoview"
@@ -32,17 +36,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.5"
     }
     packaging {
         resources {
@@ -54,6 +58,7 @@ android {
 
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -69,12 +74,64 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.android.compiler)
-    implementation(libs.androidx.paging.runtime)
-    implementation(libs.retrofit)
-    implementation(libs.androidx.paging.runtime.v212)
-    implementation(libs.logging.interceptor)
-    implementation (libs.converter.moshi)
+    implementation(libs.androidx.appcompat)
 
+    /* *****************************************************
+     **** Lifecycle
+     ****************************************************** */
+    implementation(libs.lifecycle.extensions)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.lifecycle.process)
+    implementation(libs.lifecycle.common.java8)
+    implementation(libs.runtime.livedata)
+    implementation(libs.activity.ktx)
+
+
+    /* *****************************************************
+      **** Coroutines
+      ****************************************************** */
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    /* *****************************************************
+      **** Splash
+      ****************************************************** */
+    implementation(libs.core.splashscreen)
+
+    /* *****************************************************
+     **** Lottie Animation
+     ****************************************************** */
+    implementation(libs.lottie.compose)
+
+
+    /* *****************************************************
+     **** Coil Image Loading
+     ****************************************************** */
+    implementation(libs.coil.compose)
+    implementation(libs.coil.video)
+
+    /* *****************************************************
+     **** Download File
+    ****************************************************** */
+    implementation(libs.work.runtime.ktx)
+
+    /* *****************************************************
+    **** Dependency-Hilt Injection
+    ****************************************************** */
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.work)
+
+    /* *****************************************************
+      **** Retrofit2
+      ****************************************************** */
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.converter.moshi)
 }
